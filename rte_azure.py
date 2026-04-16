47c681e3373a71f92abddbd32b6dfa759138b21b
Billy Newport <billy@billynewport.com>
2026-04-16 12:52:56 -0400

    Update rte_azure.py

--- rte_azure.py
+++ rte_azure.py
@@ -27,7 +27,7 @@ MERGE_DBNAME: str = "merge_db"
 AIRFLOW_HOST: str = "PLACEHOLDER_PG_FQDN"
 AIRFLOW_PORT: int = 5432
 AIRFLOW_SERVICE_ACCOUNT: str = "airflow-worker"
-DATASURFACE_VERSION: str = "1.1.0"
+DATASURFACE_VERSION: str = "1.3.7"
 
 
 def createDemoPSP() -> YellowPlatformServiceProvider:
